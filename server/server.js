0d1f541bfe99478ea108363eb6eea2e89edf82dc
blinozavrr <146163466+blinozavrr@users.noreply.github.com>
2025-02-25 00:54:00 +0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const connectDB = require('./config/db');
 
@@ -12,21 +13,26 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Подключаемся к БД
 connectDB();
 
-// Тестовый роут
-app.get('/', (req, res) => {
-  res.send('Server is running...');
-});
-
-// Подключаем роуты
+// Подключаем роуты API
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/tasks', taskRoutes);
 
-// Обработка ошибок
+// Раздаём статические файлы из React-сборки (client/build)
+app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+
+// Любые "не-API" GET-запросы будут возвращать index.html из папки build
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
+});
+
+// Глобальная обработка ошибок
 app.use(errorHandler);
 
+// Запуск сервера
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
